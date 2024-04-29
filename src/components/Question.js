6c8f75bfbbab31f240913db486e45887ba74dde0
Nelson Mecha <nelsonorinamecha@gmail.com>
2024-04-29 18:00:43 +0300

    use-effect hook

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect,useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() =>{
+    setTimeout(()=> {
+    setTimeRemaining((timeRemaining) => timeRemaining - 1)
+  },1000)
+   return function () {
+    clearTimeout()
+   }
+  },
 
+  [timeRemaining]
+  )
+
+  if(timeRemaining < 1){
+    setTimeRemaining(10)
+    onAnswered(false)
+  }
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
 
   const { id, prompt, answers, correctIndex } = question;
-
+//console.log(prompt);
   return (
     <>
       <h1>Question {id}</h1>
